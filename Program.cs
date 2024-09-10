6d708b48e5448d0c205e70536b57cc818ae91b28
jolahmilovski@gmail.com <jolahmilovski@gmail.com>
2024-09-10 23:35:41 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace IntroductionInLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input lenth of array ");
+            int lengthOfArrey =Convert.ToInt32(Console.ReadLine());
+            int[] number = new int[lengthOfArrey];
+
+            Random random = new Random();
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = random.Next(-number.Length, number.Length);
+            }
+
+            var arrayNumber =string.Join(", ", number);
+            Console.WriteLine($"You array is {arrayNumber}");
+
+            int sumNumber = number.Sum();
+            Console.WriteLine($"Sum of numbers in your array is {sumNumber}");
+
+            List<int> listOfNumber = new List<int>() { };
+            listOfNumber.AddRange(number);
+            var sortNumber = listOfNumber.OrderBy(x => x);
+            Console.WriteLine(string.Join(", ", sortNumber));
+        }
+    }
+}
